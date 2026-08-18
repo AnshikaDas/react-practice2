a53721aca894f7df6daca91ca6d46e5ae869a4d5
Anshika Das <anshikadas026@gmail.com>
2026-08-19 00:46:32 +0530

    Initial commit

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import StarRating from './components/star-rating'
+import LoadMoreData from "./components/load-more-data";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <div className='App'>
-    <StarRating noOfStars={10}/>
+   {/* <StarRating noOfStars={10}/>*/}
+   <LoadMoreData/>
   </div>
   )
 }
