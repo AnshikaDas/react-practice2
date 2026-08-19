2e1040a1a0f8fd60c63a07a07b6a41311205cd3c
Anshika Das <anshikadas026@gmail.com>
2026-08-19 20:55:50 +0530

    scroll-indicator

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import StarRating from './components/star-rating'
 import LoadMoreData from "./components/load-more-data";
 import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
   <div className='App'>
    {/* <StarRating noOfStars={10}/>*/}
   {/* <LoadMoreData/> */}
-   <LightDarkMode/>
+   {/*LightDarkMode/>  */}
+   <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
   </div>
   )
 }
